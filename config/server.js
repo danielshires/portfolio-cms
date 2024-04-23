68e3e989420178f0665c604301d46a754ed86729
danielshires <hello@danielshires.com>
2024-04-23 13:24:08 +1000

    Updated JSConfig and Server file

--- config/server.js
+++ config/server.js
@@ -1,23 +1,8 @@
-const crypto = require('crypto');
-
-// Generate a random key
-const generateKey = () => {
-  return crypto.randomBytes(32).toString('hex');
-};
-
-// Example usage
-const keyA = generateKey();
-const keyB = generateKey();
-
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  server: {
-    keys: [generateKey(), generateKey()]
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
