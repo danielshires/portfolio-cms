18dd855ea2a764d2ce8d5febfcf8d6e18743cf8d
danielshires <hello@danielshires.com>
2024-04-23 13:17:41 +1000

    Added generated server keys to Server.JS

--- config/server.js
+++ config/server.js
@@ -1,3 +1,15 @@
+const crypto = require('crypto');
+
+// Generate a random key
+const generateKey = () => {
+  return crypto.randomBytes(32).toString('hex');
+};
+
+// Example usage
+const keyA = generateKey();
+const keyB = generateKey();
+
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -5,7 +17,7 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
   server: {
-    keys: env.array('APP_KEYS'),
+    keys: [generateKey(), generateKey()]
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
